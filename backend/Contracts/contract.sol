4c55754da5857d9f668f384c81ad0e3a6a9e9a84
Rushhaabhhh <rushabhmistry16@gmail.com>
2024-10-23 23:26:47 +0530

    Contract

--- backend/Contracts/contract.sol
+++ backend/Contracts/contract.sol
@@ -1,71 +1,71 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+// // SPDX-License-Identifier: MIT
+// pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
+// import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+// import "@openzeppelin/contracts/access/Ownable.sol";
+// import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract DigitalAssetMarketplace is ERC721URIStorage, Ownable {
-    using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
+// contract DigitalAssetMarketplace is ERC721URIStorage, Ownable {
+//     using Counters for Counters.Counter;
+//     Counters.Counter private _tokenIds;
 
-    struct Asset {
-        uint256 id;
-        address creator;
-        uint256 price;
-        bool isForSale;
-    }
+//     struct Asset {
+//         uint256 id;
+//         address creator;
+//         uint256 price;
+//         bool isForSale;
+//     }
 
-    mapping(uint256 => Asset) public assets;
+//     mapping(uint256 => Asset) public assets;
 
-    event AssetListed(uint256 indexed tokenId, address indexed seller, uint256 price);
-    event AssetSold(uint256 indexed tokenId, address indexed seller, address indexed buyer, uint256 price);
+//     event AssetListed(uint256 indexed tokenId, address indexed seller, uint256 price);
+//     event AssetSold(uint256 indexed tokenId, address indexed seller, address indexed buyer, uint256 price);
 
-    constructor() 
-        ERC721("DigitalAsset", "DA")  
-        Ownable(msg.sender) { }     
+//     constructor() 
+//         ERC721("DigitalAsset", "DA")  
+//         Ownable(msg.sender) { }     
 
 
-    function createAsset(string memory tokenURI, uint256 price) public returns (uint256) {
-        _tokenIds.increment();
-        uint256 newItemId = _tokenIds.current();
-        _safeMint(msg.sender, newItemId);
-        _setTokenURI(newItemId, tokenURI);
+//     function createAsset(string memory tokenURI, uint256 price) public returns (uint256) {
+//         _tokenIds.increment();
+//         uint256 newItemId = _tokenIds.current();
+//         _safeMint(msg.sender, newItemId);
+//         _setTokenURI(newItemId, tokenURI);
         
-        assets[newItemId] = Asset(newItemId, msg.sender, price, true);
+//         assets[newItemId] = Asset(newItemId, msg.sender, price, true);
         
-        emit AssetListed(newItemId, msg.sender, price);
+//         emit AssetListed(newItemId, msg.sender, price);
         
-        return newItemId;
-    }
+//         return newItemId;
+//     }
 
-    function buyAsset(uint256 tokenId) public payable {
-        Asset storage asset = assets[tokenId];
-        require(asset.isForSale, "Asset is not for sale");
-        require(msg.value >= asset.price, "Insufficient payment");
+//     function buyAsset(uint256 tokenId) public payable {
+//         Asset storage asset = assets[tokenId];
+//         require(asset.isForSale, "Asset is not for sale");
+//         require(msg.value >= asset.price, "Insufficient payment");
         
-        address seller = ownerOf(tokenId);
+//         address seller = ownerOf(tokenId);
         
-        _transfer(seller, msg.sender, tokenId);
-        payable(seller).transfer(msg.value);
+//         _transfer(seller, msg.sender, tokenId);
+//         payable(seller).transfer(msg.value);
         
-        asset.isForSale = false;
+//         asset.isForSale = false;
         
-        emit AssetSold(tokenId, seller, msg.sender, msg.value);
-    }
+//         emit AssetSold(tokenId, seller, msg.sender, msg.value);
+//     }
 
-    function listAssetForSale(uint256 tokenId, uint256 price) public {
-        require(ownerOf(tokenId) == msg.sender, "Not the owner");
+//     function listAssetForSale(uint256 tokenId, uint256 price) public {
+//         require(ownerOf(tokenId) == msg.sender, "Not the owner");
         
-        assets[tokenId].price = price;
-        assets[tokenId].isForSale = true;
+//         assets[tokenId].price = price;
+//         assets[tokenId].isForSale = true;
         
-        emit AssetListed(tokenId, msg.sender, price);
-    }
+//         emit AssetListed(tokenId, msg.sender, price);
+//     }
 
-    function cancelListing(uint256 tokenId) public {
-        require(ownerOf(tokenId) == msg.sender, "Not the owner");
+//     function cancelListing(uint256 tokenId) public {
+//         require(ownerOf(tokenId) == msg.sender, "Not the owner");
         
-        assets[tokenId].isForSale = false;
-    }
-}
+//         assets[tokenId].isForSale = false;
+//     }
+// }
